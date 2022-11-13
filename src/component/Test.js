c65b2767713c452049e0b85336b780522287aa81
Maksym <seokont@gmail.com>
2022-11-13 20:28:07 +0200

    fixed 28

--- src/component/Test.js
+++ src/component/Test.js
@@ -12,7 +12,7 @@ const TestPage = () => {
         }
         return [count, increment, decrement]
     }
-    const [count, increment, decrement] = useCounter(val, 7000)
+    const [count, increment, decrement] = useCounter(val, 8000)
     return (
         <>
             <button onClick={increment}> +++</button>
