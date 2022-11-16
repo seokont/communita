9eb05755a32d1d859a6e998e17f6e3563a6f8956
Maksym <seokont@gmail.com>
2022-11-16 22:00:01 +0200

    fixed 38

--- src/component/Test.js
+++ src/component/Test.js
@@ -1,24 +1,35 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 const TestPage = () => {
-    const val = 1980
-    const useCounter = (values = 0, delta = 1) => {
-        const [count, setCount] = useState(values)
-        const increment = () => {
-            setCount((prevState) => prevState + delta)
-        }
-        const decrement = () => {
-            setCount((prevState) => prevState - delta)
-        }
-        return [count, increment, decrement]
-    }
-    const [count, increment, decrement] = useCounter(val, 8000)
-    return (
-        <>
-            <button onClick={increment}> +++</button>
-            <div>{count}</div>
-            <button onClick={decrement}> ---</button>
-        </>
-    )
-}
+  const valk = 1980;
+  const [val, setVal] = useState(1980);
+
+  useEffect(() => {
+    console.log("didmount");
+    return () => {
+      console.log("will");
+    };
+  }, [val]);
+
+  const useCounter = (values = 0, delta = 1) => {
+    const [count, setCount] = useState(values);
+    const increment = () => {
+      setCount((prevState) => prevState + delta);
+    };
+    const decrement = () => {
+      setCount((prevState) => prevState - delta);
+    };
+    return [count, increment, decrement];
+  };
+  const [count, increment, decrement] = useCounter(valk, 8000);
+  return (
+    <>
+      <button onClick={increment}> +++</button>
+      <div>{count}</div>
+      <button onClick={decrement}> ---</button>
+
+      <div onClick={() => setVal((prev) => prev + 1)}>{val}</div>
+    </>
+  );
+};
 export default TestPage;
